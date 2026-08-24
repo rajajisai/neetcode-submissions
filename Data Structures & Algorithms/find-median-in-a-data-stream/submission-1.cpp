83c1ac235c57515d8567efddcabab12fbed0e019
rajajisai <40017333+rajajisai@users.noreply.github.com>
2026-08-24 11:35:33 -0400

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.cpp
@@ -0,0 +1,37 @@
+class MedianFinder {
+private:
+priority_queue<int> max_heap;
+priority_queue<int,vector<int>,greater<int>> min_heap;
+public:
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        max_heap.push(num);
+        if (!min_heap.empty() && max_heap.top()>min_heap.top()){
+            min_heap.push(max_heap.top());
+            max_heap.pop();
+        }
+        if (min_heap.size()>max_heap.size()+1){
+            max_heap.push(min_heap.top());
+            min_heap.pop();
+        }
+        if (max_heap.size()>min_heap.size()+1){
+            min_heap.push(max_heap.top());
+            max_heap.pop();
+        }
+    }
+    
+    double findMedian() {
+        if (min_heap.size()==max_heap.size()){
+            return (min_heap.top()+max_heap.top())/2.0;
+        }
+        if (min_heap.size()>max_heap.size()){
+            return min_heap.top();
+        }
+        if (max_heap.size()>min_heap.size()){
+            return max_heap.top();
+        }
+    }
+};
