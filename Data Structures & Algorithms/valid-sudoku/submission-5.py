10368f24ed02b02277ca68a42b5ca74f13cdff04
rajajisai <40017333+rajajisai@users.noreply.github.com>
2026-09-12 17:40:06 -0400

    Add: valid-sudoku - submission-5

---
+++ Data Structures & Algorithms/valid-sudoku/submission-5.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        for i in range(0,9,3):
+            for j in range(0,9,3):
+
+                count=[0]*10
+                for k in range(0,3):
+                    for l in range(0,3):
+                        if (board[i+k][j+l] is not '.' and count[int(board[i+k][j+l])]==1):
+                            return False
+                        elif  board[i+k][j+l] is not '.':
+                            count[int(board[i+k][j+l])]=1
+        
+        for i in range(0,9):
+            count=[0]*10
+            for j in range(0,9):
+                if (board[i][j] is not '.' and count[int(board[i][j])]==1):
+                    return False
+                elif board[i][j] is not '.':
+                    count[int(board[i][j])]+=1
+        
+        for j in range(0,9):
+            count=[0]*10
+            for i in range(0,9):
+                if (board[i][j] is not '.' and count[int(board[i][j])]==1):
+                    return False
+                elif board[i][j] is not '.':
+                    count[int(board[i][j])]+=1
+        
+        return True
+                
+        
+
+        
